5e0de029fb8e5cdcbaf85aa032547a6be29c44da
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-07-03 22:37:45 +0530

    open api config

---
+++ book-network/src/main/java/com/shanuka/book_network/security/OpenApiConfig.java
@@ -0,0 +1,48 @@
+package com.shanuka.book_network.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "shanuka",
+                        email = "contact@shanuka.com",
+                        url = ""
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "Open Api Specification - shanuka",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "someurl"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = ""
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
